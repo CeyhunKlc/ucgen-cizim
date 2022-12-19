7a747de566f0944e4040d3d2312b3deaab506d88
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-12-19 14:30:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ucgencizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kenar Uzunluğu:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int sayac = 1;
+            int kenaruzunlugu = length;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = kenaruzunlugu-1; j >0; j--)
+                {
+                    Console.Write(" ");
+                }
+                kenaruzunlugu--;
+                for (int k = 1; k <= sayac; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                sayac++;
+                
+                Console.WriteLine();
+
+
+
+            }
+        }
+    }
+}
